1cd9d940a96b062496026ba48167b3e9e4245d45
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-18 12:45:21 +0200

    add comments, clean code

--- script.js
+++ script.js
@@ -23,24 +23,25 @@ const model = {
     ]
 }
 
+// operator object 
 const octopus = {
-    "myTimeOut" : "",
-    "clearWrapperTimeOut" : "",
+    "carouselTimeOut" : "",
     "changeRotationTimeOutShow" : "",
     "changeRotationTimeOutHide" : "",
     "rotationToBottomTimeOut" : "",
-
+// catching wrapper
     "getWrapper" : function() {
         var wrapper = document.getElementById('wrapper');
         return wrapper;
     },
+// creating img and setting position
     "createImg" : function() {
         var pic = document.createElement('img');
         pic.id = 'pic'
         pic.style.transform = 'rotateX(-90deg)';
         return pic;
     },
-
+// getting img from DOM or creating new one && setting wrapper persoective
     "getImg" : function() {
         var wrapper = octopus.getWrapper(); 
         wrapper.style.perspectiveOrigin = "top";
@@ -54,29 +55,31 @@ const octopus = {
         return pic;
 
     },
+// setting correct img based on given value 'x'
     "setImg" : function(x) {
         var pic = octopus.getImg();
         var wrapper = octopus.getWrapper();
         wrapper.appendChild(pic);
         pic.setAttribute('src', model.pics[x].imgSrc);
         pic.setAttribute('alt', model.pics[x].imgAlt);
-        octopus.changeRotation();
+        octopus.changeRotation(pic);
     },
+// after clicking indicator clearing all intervals and wrapper, starting again from clicked indicator
     "manualSetImg" : function(index) {
         var wrapper = octopus.getWrapper();
         wrapper.innerHTML = "";
-        clearTimeout(octopus.myTimeOut);
-        clearTimeout(octopus.clearWrapperTimeOut);
+        clearTimeout(octopus.carouselTimeOut);
         clearTimeout(octopus.changeRotationTimeOutShow);
         clearTimeout(octopus.changeRotationTimeOutHide);
         clearTimeout(octopus.rotationToBottomTimeOut);
         octopus.setImg(index);
         octopus.carousel(index);
     },
+// calling changeImg after 3s
     "carousel" : function(x) {
-        octopus.myTimeOut = setTimeout(function() {octopus.changeImg(x)}, 3000);
+        octopus.carouselTimeOut = setTimeout(function() {octopus.changeImg(x)}, 3000);
     },
-
+// changing img, adding active class to indicator and calling carousel again
     "changeImg" : function(x) {
         var x = x;
         if (x == (model.pics.length-1)) {
@@ -91,33 +94,36 @@ const octopus = {
             octopus.carousel(x);
         }
     },
-    "changeRotation" : function() {
-        var pic = octopus.getImg();
+// change img rotation from top to middle with timeout for better effect
+    "changeRotation" : function(pic) {
         octopus.changeRotationTimeOutShow = setTimeout(function() {
             pic.style.transform = "rotateX(-0.0001deg)";
-            var wrapper = octopus.getWrapper();
             octopus.changeRotationTimeOutHide = setTimeout(function() {
-                octopus.rotationToBottom();
+                octopus.rotationToBottom(pic);
             }, 1350);
         }, 100);
     },
-    "rotationToBottom" : function() {
-        var pic = octopus.getImg();
+// change img rotation from middle to bottom with timeout 
+// changing wrapper and img perspectife for better effect
+// calling clearWrapper
+    "rotationToBottom" : function(pic) {
         var wrapper = octopus.getWrapper();
         wrapper.style.perspectiveOrigin = "bottom";
         pic.style.transformOrigin = "bottom";
         pic.style.transform = "rotateX(0.0001deg)";
         octopus.rotationToBottomTimeOut = setTimeout(function() {
             pic.style.transform = "rotateX(90deg)";
-            octopus.clearWrapper();
+            octopus.clearWrapper(wrapper, pic);
         }, 800)
     },
-    "clearWrapper" : function() {
-        var wrapper = octopus.getWrapper();
-        octopus.clearWrapperTimeOut = setTimeout(function() {
-            wrapper.innerHTML = "";
-        }, 700)
+// deleting img from wrapper after img finishes transition
+    "clearWrapper" : function(wrapper, pic) {
+        pic.addEventListener('transitionend', function() {
+            wrapper.innerHTML = "";            
+        });
     },
+
+// creating a list of indicators based on how many imgs there is in a model.pics
     "createLIs" : function() {
         var UL = document.getElementById('UL');
         for (var i = 0; i <= model.pics.length-1; i++) {
@@ -126,12 +132,13 @@ const octopus = {
             UL.appendChild(LI);
         }
     },
+// add class 'active' to indicate which img is beeing shown
     "addActiveClass" : function(x) {
         var indicators = Array.from(document.getElementsByClassName('indicator'));
         indicators.forEach(indicator => indicator.classList.remove('active'));
         indicators[x].classList.add('active');
-        
     },
+// listens for clicking indicator
     "eventListeners" : function() {
         const indicators = Array.from(document.getElementsByClassName('indicator'));
         indicators.forEach(indicator => indicator.addEventListener('click', function() {
@@ -144,6 +151,7 @@ const octopus = {
 }
 
 
+// displaying slider and setting carousel
 const view = {
     displayPic : function() {
         octopus.setImg(0);
@@ -154,4 +162,5 @@ const view = {
     }
 }
 
+// firing up
 view.displayPic();
